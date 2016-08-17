3569146271d5f3275994aa7fd3327b110f0ed885
Lisa Solovyeva <vasilisa.solovyeva@gmail.com>
2016-08-17 11:35:54 +0200

    First commit

---
+++ src/Main.java
@@ -0,0 +1,60 @@
+import com.googlecode.lanterna.*;
+import com.googlecode.lanterna.input.Key;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.nio.charset.Charset;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Player player = new Player(10, 10);
+
+        Terminal terminal = TerminalFacade.createTerminal(System.in, System.out, Charset.forName("UTF8"));
+        terminal.enterPrivateMode();
+        while (true) {
+            UpdateScreen(player, terminal);
+            MovePlayer(player, terminal);
+        }
+    }
+
+    private static void UpdateScreen(Player player, Terminal terminal) {
+        terminal.clearScreen();
+        terminal.moveCursor(player.x, player.y);
+        terminal.putCharacter('O');
+        terminal.moveCursor(0,0);
+    }
+
+    public static void MovePlayer (Player player, Terminal terminal) throws InterruptedException {
+        Key key;
+        do {
+            Thread.sleep(5);
+            key = terminal.readInput();
+
+            switch (key.getCharacter()){
+                case 'U':
+                    if (player.y > 0) {
+                        player.y--;
+                    }
+                    break;
+                case 'D':
+                    if (player.y < 20) {
+                        player.y++;
+                    }
+                    break;
+                case 'L':
+                    if (player.x > 0) {
+                        player.x--;
+                    }
+                    break;
+                case 'R':
+                    if (player.x < 20) {
+                        player.y++;
+                    }
+                    break;
+            }
+
+        } while (key == null);
+        System.out.println(key.getCharacter() + " " + key.getKind());
+    }
+}
---
+++ src/Player.java
@@ -0,0 +1,12 @@
+/**
+ * Created by vasil on 2016-08-17.
+ */
+public class Player {
+    public int x = 0;
+    public int y = 0;
+
+    public Player(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
