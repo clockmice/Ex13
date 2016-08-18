303d2bee153859543d05d7d71dbb4641a41d57ae
Lisa Solovyeva <vasilisa.solovyeva@gmail.com>
2016-08-18 12:54:59 +0200

    Third Commit

---
+++ src/Color.java
@@ -0,0 +1,14 @@
+/**
+ * Created by vasil on 2016-08-18.
+ */
+public class Color {
+    int color1;
+    int color2;
+    int color3;
+
+    public Color(int color1, int color2, int color3) {
+        this.color1 = color1;
+        this.color2 = color2;
+        this.color3 = color3;
+    }
+}
