02b693e8808dcaf480c8663013038ddd333301e3
Lisa Solovyeva <vasilisa.solovyeva@gmail.com>
2016-08-19 15:36:24 +0200

    Monster Game commit1

---
+++ src/FastMonster.java
@@ -0,0 +1,8 @@
+/**
+ * Created by vasil on 2016-08-19.
+ */
+public class FastMonster extends Monster {
+    public FastMonster(float x, float y) {
+        super(x, y);
+    }
+}
---
+++ src/SlowMonster.java
@@ -0,0 +1,8 @@
+/**
+ * Created by vasil on 2016-08-19.
+ */
+public class SlowMonster extends Monster {
+    public SlowMonster(float x, float y) {
+        super(x, y);
+    }
+}
